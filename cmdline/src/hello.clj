43390830186a39d270be665f8697f983a23af1ee
leapon <leapon@gmail.com>
2018-08-29 11:40:40 -0400

    list bucket call added in hello

--- cmdline/src/hello.clj
+++ cmdline/src/hello.clj
@@ -1,6 +1,13 @@
 (ns hello
+  (:import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+           java.io.File)
   (:require [clj-time.core :as t]
-            [clj-time.format :as f]))
+            [clj-time.format :as f]
+            [clojure-helpers.aws.s3 :as s3-utils])
+  (:use [clojure.set]
+        [clojure.pprint]
+        [amazonica.core]
+        [amazonica.aws.s3]))
 
 (defn time-str
   "Returns a string representation of a datetime in the local time zone."
@@ -9,6 +16,14 @@
     (f/with-zone (f/formatter "hh:mm aa") (t/default-time-zone))
     dt))
 
-(defn -main []
-  (println "Hello world, the time is" (time-str (t/now))))
+(defn do-s3-upload []
+  ;(def upload-file   (java.io.File. "upload.txt"))
+  ;(s3-utils/put-file-to-s3 ["xml-upload/testxml-367.txt" "test367" "csa-data-test"])
+  (let [buckets (list-buckets (DefaultAWSCredentialsProviderChain.))]
+    (println "buckets:" buckets))
+  (println "do s3 upload test"))
 
+
+(defn -main []
+  (do-s3-upload)
+  (println "Hello world, the time is" (time-str (t/now))))
